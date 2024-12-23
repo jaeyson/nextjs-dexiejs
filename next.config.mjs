847e7f733d7aa012c0fab71cf764e9d5703d64d0
jaeyson <hello@jaeyson.dev>
2024-12-23 11:36:27 +0800

    fix broken image link

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: isProd ? '/nextjs-dexiejs' : '',
+  basePath: isProd ? '/nextjs-dexiejs' : ''
 };
 
 export default nextConfig;
